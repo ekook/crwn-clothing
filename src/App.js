c763b6d32e975fbfce962e6695245d48f28c22d7
ekook <edselkook@gmail.com>
2022-09-08 20:12:46 -0400

    add routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
-import './categories.styles.scss'
+import {Routes, Route} from 'react-router-dom'
 
-import Directory from './components/directory/directory.component';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
-const App = () => {
 
-    const categories = [
-  {
-    "id": 1,
-    "title": "Hats",
-    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    "id": 2,
-    "title": "Jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    "id": 3,
-    "title": "Sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    "id": 4,
-    "title": "Womens",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    "id": 5,
-    "title": "Mens",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-  }
-]
+
+const Shop = () => {
   return (
-      <Directory categories={categories}/>
+    <h1>The shop</h1>
   )
 }
 
-export default App;
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+         <Route index element={<Home />}/>
+         <Route path='shop' element={<Shop />}/>
+      </Route>
+
+    </Routes>
+  )
+}
+
+export default App
